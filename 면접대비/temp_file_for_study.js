bfd22d832c49ffd1a5e3f6fa966cc908f034391e
Yeonghwan Park (John Park) <vyckd354@gmail.com>
2023-09-04 19:25:49 +0900

    Update temp_file_for_study.js

--- 면접대비/temp_file_for_study.js
+++ 면접대비/temp_file_for_study.js
@@ -2,7 +2,16 @@
 // 좋은 코드는 Readible(가독성)하고 Scalable(Big O)해야함
 // Big O는 알고리즘이 실행되는데 걸리는 시간 (낮을수록 더 효율적이고 빠른 코드)
 
-// ############## 코드 예시
+// 시간 복잡도(Time Complexity): 입력된 N의 크기에 따라 실행되는 조작의 수를 나타낸다.
+// 공간 복잡도(Space Complexity): 알고리즘이 실행될 때 사용하는 메모리의 양을 나타낸다.
+  
+// ############## Big O 계산의 4가지 규칙
+// Worst Case
+// Remove Constants
+// Different Terms For Inputs
+// Drop Non Dominants
+
+// ############## O(n) 코드 예시
 const everyone = ['1', '2', '3', '4', '5', '6', 'nemo', '7', '8', '9'];
 const large = new Array(100000).fill('nemo');
 
@@ -17,6 +26,30 @@ const findNemo = (array) => {
   console.log('call to find Nemo took', +(t1 - t0) + 'ms');
 };
 
-findNemo(large);  // O(n) -> linear time (딱 배열이 길어진 만큼만 시간이 걸림, '/' 형태의 그래프)
+findNemo(large);  // O(n) --> linear time (딱 배열이 길어진 만큼만 시간이 걸림, '/' 형태의 그래프)
 
-// ############## 코드 예시
+// ############## O(1) 코드 예시
+
+const boxes = [0, 1, 2, 3, 4, 5, 6];
+const logFirstTwoBoxes = (boxes) => {
+  console.log(boxes[0]);  //  O(1)
+  console.log(boxes[1]);  //  O(1)
+};
+
+logFirstTwoBoxes(boxes);  //  O(2) but O(1)로 봄 --> constant time (딱 배열이 길어져도 정해진 시간만 걸림, 'ㅡ' 형태의 그래프)
+
+// ############## O(n^2) 코드 예시
+const boxes = ['a', 'b', 'c', 'd', 'e'];
+
+const logAllPairsOfArray = (array) => {
+    // O(n) * O(n)
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      console.log(array[i], array[j]);
+    }
+  }
+};
+
+logAllPairsOfArray(boxes); // O(n^2) --> Quadratice time
+
+// ##############  #####################################################################################################################################################################
