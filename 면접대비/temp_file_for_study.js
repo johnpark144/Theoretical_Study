b03fd1943ec0cb1ea1aa3b1392ed887695879951
Yeonghwan Park (John Park) <vyckd354@gmail.com>
2023-09-04 17:55:21 +0900

    Update temp_file_for_study.js

--- 면접대비/temp_file_for_study.js
+++ 면접대비/temp_file_for_study.js
@@ -2,7 +2,7 @@
 // 좋은 코드는 Readible(가독성)하고 Scalable(Big O)해야함
 // Big O는 알고리즘이 실행되는데 걸리는 시간 (낮을수록 더 효율적이고 빠른 코드)
 
-// 비효율적인 코드
+// ############## 코드 예시
 const everyone = ['1', '2', '3', '4', '5', '6', 'nemo', '7', '8', '9'];
 const large = new Array(100000).fill('nemo');
 
@@ -17,4 +17,6 @@ const findNemo = (array) => {
   console.log('call to find Nemo took', +(t1 - t0) + 'ms');
 };
 
-findNemo(large);
+findNemo(large);  // O(n) -> linear time (딱 배열이 길어진 만큼만 시간이 걸림, '/' 형태의 그래프)
+
+// ############## 코드 예시
