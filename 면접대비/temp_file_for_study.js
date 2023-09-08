530782afbcaa10fe1bc71085fae4461a7b8f4c21
Yeonghwan Park (John Park) <vyckd354@gmail.com>
2023-09-08 22:38:17 +0900

    Update temp_file_for_study.js

--- 면접대비/temp_file_for_study.js
+++ 면접대비/temp_file_for_study.js
@@ -822,16 +822,56 @@ const graph4 = {
 };
 
 // ########################################################################################################################################################## 그래프 구현  ##############
-// ############## 
+// ############## Adjacent List
+class Graph {
+  constructor() {
+    this.numberOfNodes = 0;
+    this.adjacentList = {};
+  }
+  // 노드 추가
+  addVertex(node) {
+    this.adjacentList[node] = [];
+    this.numberOfNodes++;
+  }
+  // 각 노드끼리 연결
+  addEdge(node1, node2) {
+    //uniderected Graph
+    this.adjacentList[node1].push(node2);
+    this.adjacentList[node2].push(node1);
+  }
+  // 연결된 노드들 확인
+  showConnections() {
+    const allNodes = Object.keys(this.adjacentList);
+    for (let node of allNodes) {
+      let nodeConnections = this.adjacentList[node];
+      let connections = '';
+      let vertex;
+      for (vertex of nodeConnections) { 
+        connections += vertex + ' ';
+      }
+      console.log(node + '-->' + connections);
+    }
+  }
+}
 
+var myGraph = new Graph();
+myGraph.addVertex('0');
+myGraph.addVertex('1');
+myGraph.addVertex('2');
+myGraph.addVertex('3');
+myGraph.addVertex('4');
+myGraph.addVertex('5');
+myGraph.addVertex('6');
+myGraph.addEdge('3', '1');
+myGraph.addEdge('3', '4');
+myGraph.addEdge('4', '2');
+myGraph.addEdge('4', '5');
+myGraph.addEdge('1', '2');
+myGraph.addEdge('1', '0');
+myGraph.addEdge('0', '2');
+myGraph.addEdge('6', '5');
 
-
-
-
-
-
-
-
+myGraph.showConnections();
 
 
 // #########################################################################################################################################################################################
