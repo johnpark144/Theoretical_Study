5a7cc7a0fe597f3223ce2bc2003acb67360b4d32
Yeonghwan Park (John Park) <vyckd354@gmail.com>
2023-09-13 04:03:50 +0900

    Update temp_file_for_study.js

--- 면접대비/temp_file_for_study.js
+++ 면접대비/temp_file_for_study.js
@@ -1372,4 +1372,48 @@ console.log(1, memoized(6));
 console.log(2, memoized(2));
 console.log(2, memoized(2));
 
-// ############## 클로저를 이용한 캐싱
+// ##############  
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233...
+let calculations = 0;
+
+const fibonacci = (n) => {   // O(2^n)
+  calculations++; // 몇번 불렸는지 계산
+
+  if (n < 2) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+const fibonacciMaster = () => {   // O(n)
+  let cache = {};
+  return function fib(n) {
+    if (n in cache) {
+      return cache[n];
+    } else {
+      if (n < 2) {
+        return n;
+      } else {
+        cache[n] = fib(n - 1) + fib(n - 2);
+        return cache[n];
+      }
+    }
+  };
+};
+
+const fibonacciMaster2 = (n) => {
+  let answer = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    answer.push(answer[i - 2] + answer[i - 1]);
+  }
+  return answer.pop();
+};
+
+const fasterFib = fibonacciMaster();
+
+console.log('Slow', fibonacci(35));
+console.log('DP', fasterFib(100));
+console.log('DP2', fibonacciMaster2(100));
+console.log('we did ' + calculations + ' calculations');
+
+
