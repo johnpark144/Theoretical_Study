4b1c01bfaff8cf7ad5b4ab6b602df0b7d7b21164
Yeonghwan Park (John Park) <vyckd354@gmail.com>
2023-09-14 02:16:31 +0900

    Update Counter.js

--- Study_for_CodingTest/Counter.js
+++ Study_for_CodingTest/Counter.js
@@ -1,1 +1,35 @@
-zz;
+// ########### Questions #############################################################################
+// Make the text within the button display the number of times the button has been clicked.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ########### Solution #############################################################################
+import { useState } from 'react';
+
+import './styles.css';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>
+        Clicks: {count}
+      </button>
+    </div>
+  );
+}
