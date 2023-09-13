1efc2cf5500997b91c5c4d65cf2bc95febb70722
Yeonghwan Park (John Park) <vyckd354@gmail.com>
2023-09-13 18:36:18 +0900

    Update 5_Data_structure_Algorithm.js

--- Study_for_Question/5_Data_structure_Algorithm.js
+++ Study_for_Question/5_Data_structure_Algorithm.js
@@ -6,8 +6,8 @@
 // 알고리즘 -- 재귀, 정렬, 탐색, BFS, DFS, 다이나믹 프로그래밍
 
 // ############## Big O notation #####################################################################################################################################################################
-// 좋은 코드는 1) No Repeat(중복성 없음), Consistent(일관성있는), Readible(가독성), Teastable(테스트하기 좋음)  2) Scalable(Big O) ---> 2-1)속도(시간복잡도 less than O(N^2)),  2-2)메모리(공간복잡도)
-// Scalable 즉, 시간복잡도, 공간복잡도가 아무리 좋아도 가독성이 안좋으면 상황에 따라 좋은 코드라 볼 수없기 때문에 트레이드오프를 잘 고려 해야함.
+// 좋은 코드는 1) No Repeat(중복성 없음), Consistent(일관성있는), Readible(가독성), Testable(테스트하기 좋음)  2) Scalable(Big O) ---> 2-1)속도(시간복잡도 less than O(N^2)),  2-2)메모리(공간복잡도)
+// 시간복잡도, 공간복잡도, 가독성 트레이드오프를 잘 고려 해야함.
 
 // 시간 복잡도(Time Complexity): 입력된 N의 크기에 따라 실행되는 조작의 수
 // 공간 복잡도(Space Complexity): 사용하는 메모리의 양
@@ -27,7 +27,7 @@
 // ############## Big O 계산의 4가지 규칙
 // Worst Case (최악의 상황을 기준)
 // Remove Constants  (상수 제거)
-// Different Terms For Inputs  (입력값의 대한 분리)
+// Different Terms For Inputs  (입력 값의 대한 분리)
 // Drop Non Dominants (상대적으로 비 지배적인부분 제거)
 
 // ############## O(n) 코드 예시
@@ -79,15 +79,17 @@ logAllPairsOfArray(boxes); // O(n^2) --> Quadratice time
 // ############### 자료구조 #################################################################################################################################################################
 // #########################################################################################################################################################################################
 // ############## 배열 (Arrays) #############################################################################################################################################################
-// 문자열(String)도 split 메서드로 배열로 변경하고 join으로 원상태보 복귀 시킬 수 있어서 배열과 동일하게 적용됨
-// search O(n)    // indexOf()나 includes()같이 특정요소 검색
-// lookup  O(1)    // 특정 하나의 값만 조회
-// push/pop  O(1)    // 동적 배열은 이에 해당하나, 정적 배열은 배열 길이를 미리 정하므로 Append 할 때 0{n}이 될 수 있음 (Java, C, C++ 등)
-// insert  O(n)    // unshift도 이에 해당 --> 반복문안에서 사용하면 O(n^2)이 되서 사용 자제
-// delete  O(n)  // shift도 이에 해당
+// 문자열도 배열 같이 동일 적용 
+// 문자열(String) --> split --> 배열 --> join --> 문자열 
 
-// 장점 : 조회, push, pop에 빠르다, 순서(정렬)
-// 단점 : (push,pop을 제외한) 검색, 삽입, 삭제가 느림, 정적 배열의 경우 비어있어도 큰 용량에 고정
+// search O(n)    // indexOf()나 includes()같이 특정 요소 찾기
+// lookup  O(1)    // 특정 하나의 값만 조회
+// push/pop  O(1)    // 동적 배열: O(1), 정적 배열 : 0{n} --> 배열 길이 재 조정 (Java, C, C++ 등)
+// insert  O(n)    // unshift 포함 --> 반복문안에서 사용하면 O(n^2)이 되서 사용 자제
+// delete  O(n)  // shift 포함
+
+// 장점 : 조회, push, pop에 빠르다, 순서가 있다
+// 단점 : (push, pop을 제외한) 검색, 삽입, 삭제가 느림, 정적 배열의 경우 비어있어도 큰 용량에 고정
 // ##############
 const strings = ['a', 'b', 'c', 'd'];
 strings[2]; // O(1)
@@ -105,9 +107,9 @@ strings.splice(2, 0, '!!!'); // O(n)
 // ############## 해시 테이블 (Hash Table) #################################################################################################################################################################
 // 자바 스크립트의 객체, 파이썬의 딕셔너리가 이에 해당됨
 
-// ############## 해시 함수
+// ############## 해시 함수 란
 // 임의의 데이터를 고정된 크기의 고유한 해시 코드 또는 해시 값으로 변환하는 함수
-// 데이터를 효율적으로 저장하고 검색하는 데 사용되며, 암호학, 데이터베이스, 데이터 구조, 보안, 알고리즘 등에 사용
+// 암호학, 데이터베이스, 데이터 구조, 보안 등에 사용
 // 특징과 역할 : 고유성, 고정된 출력 크기, 빠른 계산, 충돌 최소화, 원본 추론 어려움
 
 // ##############
@@ -117,31 +119,30 @@ strings.splice(2, 0, '!!!'); // O(n)
 // search  O(1)
 
 // 장점 : 조회, 검색, 삽입, 삭제에 빠르다, 효율적인 Key사용
-// 단점 : 순서가 존재하지 않는다, 이터러블 하지않아 Key Iteration이 느리다
-// ##############
+// 단점 : 순서가 존재X , 이터러블 하지않음
 
 // ############## 링크드 리스트, 연결 리스트 (Linked Lists) #################################################################################################################################################################
-// 배열들이 서로 머리부터 꼬리까지 연결되어 있는 자료구조
-// 자바스크립트는 링크 리스트가 내장되있지 않지만 구현은 가능
+// 배열들을 머리부터 꼬리까지 연결
+// 자바스크립트는 링크 리스트가 내장 X
 
 // 전반적으로 링크드 리스트의 특징
-// -- 장점 :  배열보다 추가, 제거가 빠르다, 순서(정렬)
+// -- 장점 : 배열보다 추가 및 제거가 빠르다, 순서가 있다
 // -- 단점 : lookup이 느리다, 전반적으로 메모리가 많이든다
 
 // 싱글 링크드 리스트
 // -- 장점 : 메모리 효율, 구현이 간단
 // -- 단점 : 역방향 탐색 어려움, 접근 시간 오래 걸림
 // 더블 링크드 리스트
-// -- 장점 : 역방향 탐색 가능,
+// -- 장점 : 역방향 탐색 쉬움,
 // -- 단점 : 구현하기 복잡함, 메모리 비효율
 
 // ############## 배열과 비교
-// 배열: '조회'가 잦을때
-// 링크드 리스트: '삽입 및 삭제' 작업이 잦을때
+// 배열 : '조회(lookup)'가 잦을때
+// 링크드 리스트 : '삽입 및 삭제' 작업이 잦을때
 
 // ##############
-// prepend O(1)   // 앞에 추가 (unshift와 비슷하지만 시간복잡도면에서 효율적), 제거도 동일 시간복잡도
-// append O(1)    // 뒤에 추가(push와 비슷), 제거도 동일 시간복잡도
+// prepend O(1)   // 앞에 추가(배열의 unshift보다 효율적), 제거도 동일
+// append O(1)    // 뒤에 추가(push), 제거도 동일
 // lookup O(n)
 // insert O(n)  // 평균 O(1) --> 배열보다 빠름
 // delete O(n)  // 평균 O(1) --> 배열보다 빠름
@@ -163,31 +164,32 @@ let myLinkedList = {
 };
 
 // ############## 싱글 링크드 리스트 (Singly Linked Lists)
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
 class LinkedList {
   constructor(value) {
     this.head = { value, next: null };
-    this.tail = this.head;
+    this.tail = this.head;  // !! 참고 부분 !!
     this.length = 1;
   }
 
   // 뒤로 입력 (push 같이)
   append(value) {
-    const newNode = {
-      value,
-      next: null,
-    };
-    this.tail.next = newNode;
-    this.tail = newNode;
+    const newNode = new Node(value);
+    this.tail.next = newNode;  // this.tail.next는 객체의 내부라서 같은 주소로 연결 되어있는 this.head의 값도 같이 변경됨 (위에 !! 참고 부분 !!)
+    this.tail = newNode;  // this.tail 자체 값을 변경하면 this.head에는 영향 없음
     this.length++;
     return this;
   }
 
   // 앞으로 입력 (unshift 같이)
   prepend(value) {
-    const newNode = {
-      value,
-      next: this.head,
-    };
+    const newNode = new Node(value, this.head);
     this.head = newNode;
     this.length++;
     return this;
@@ -200,10 +202,7 @@ class LinkedList {
     } else if (index === 0) {
       return this.prepend(value);
     }
-    const newNode = {
-      value,
-      next: null,
-    };
+    const newNode = new Node(value);
     const leader = this.traverseToIndex(index - 1);
     const holdingPointer = leader.next;
     newNode.next = holdingPointer;
@@ -270,6 +269,7 @@ class LinkedList {
 
 const myLinkedList = new LinkedList(5); // 5
 myLinkedList.append(16); // 5,16
+console.log(myLinkedList);
 myLinkedList.prepend(10); // 10, 5, 16
 myLinkedList.insert(1, 99); // 10, 99, 5, 16
 myLinkedList.remove(2); // 10, 99, 16
