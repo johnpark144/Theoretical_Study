c08a9661a61b2258da82730c97cd4cc2f38a1deb
Yeonghwan Park <vyckd354@gmail.com>
2023-09-27 11:59:15 +0900

    Edited Digital Clock

--- Study_for_CodingTest/35_Digital_Clock.js
+++ Study_for_CodingTest/35_Digital_Clock.js
@@ -270,4 +270,257 @@ export default function Clock() {
 //     width: var(--segment-width);
 //   }
   
+// ########### Solution (내가한거) #############################################################################
+// ########### App.js
+import Clock from './Clock';
+
+import './styles.css';
+
+export default function App() {
+  return (
+    <div className="wrapper">
+      <Clock />
+    </div>
+  );
+}
+
+// ########### Clock.js
+import { useState, useEffect } from 'react'
+
+// 시간 생성 함수
+function generateTime() {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date())
+    }, 100)
+
+    return () => clearInterval(timer)
+  }, [])
+
+  return time
+}
+
+// 0 만들기
+function addZero(time) {
+  return time < 10 ? `0${time}` : time
+}
+
+// 숫자 만들기 함수
+function NumberGenerator({ num }) {
+  return (<div className="number-container">
+    <div className={`number number-top number-${num}-top`} />
+    <div className={`number number-bottom number-${num}-bottom`} />
+  </div>)
+}
+
+//  ':' 부분 생성 함수
+function Seperator() {
+  return<div className="seperator-container">
+  <div className="seperator" />
+  <div className="seperator" />
+  </div>
+}
+
+// 디폴트함수
+export default function Clock() {
+  const time = generateTime()
+
+  let hours = addZero(time.getHours()).toString()
+  let minutes = addZero(time.getMinutes()).toString()
+  let seconds = addZero(time.getSeconds()).toString()
+
+  return <div className="clock-container">
+    <NumberGenerator num={hours[0]} />
+    <NumberGenerator num={hours[1]} />
+    <Seperator />
+    <NumberGenerator num={minutes[0]} />
+    <NumberGenerator num={minutes[1]} />
+    <Seperator />
+    <NumberGenerator num={seconds[0]} />
+    <NumberGenerator num={seconds[1]} />
+  </div>;
+}
+
+// ########### Styles.css
+// body {
+//     font-family: sans-serif;
+//   }
   
+//   .wrapper {
+//     height: 200px;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//   }
+  
+//   .clock-container {
+//     --font-color: white;
+//     --border-size: 14px;
+  
+//     background-color: black;
+//     width: 70%;
+//     height: 80%;
+//     border: #CCC 10px solid;
+//     border-radius: 10px;
+//     color: var(--font-color);
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     padding: 0px 20px;
+//     gap: 6px;
+//   }
+  
+//   .number-container {
+//     display: flex;
+//     flex-direction: column;
+//     width: 66px;
+//   }
+  
+//   .number {
+//     border-width: var(--border-size);
+//     border-color: transparent;
+//     border-style: solid;
+//     height: 30px;
+//   }
+  
+//   .number-top {
+//     border-bottom-width: calc(var(--border-size) /2);
+//   }
+  
+//   .number-bottom {
+//     border-top-width: calc(var(--border-size) /2);
+//   }
+  
+//   .number-0-top {
+//     border-top-color: var(--font-color);
+//     border-right-color: var(--font-color);
+//     border-left-color: var(--font-color);
+//   }
+  
+//   .number-0-bottom {
+//     border-right-color: var(--font-color);
+//     border-bottom-color: var(--font-color);
+//     border-left-color: var(--font-color);
+//   }
+  
+//   .number-1-top {
+//     border-right-color: var(--font-color);
+//   }
+  
+//   .number-1-bottom {
+//     border-right-color: var(--font-color);
+//   }
+  
+//   .number-2-top {
+//     border-top-color: var(--font-color);
+//     border-right-color: var(--font-color);
+//     border-bottom-color: var(--font-color);
+//   }
+  
+//   .number-2-bottom {
+//     border-top-color: var(--font-color);
+//     border-bottom-color: var(--font-color);
+//     border-left-color: var(--font-color);
+//   }
+  
+//   .number-3-top {
+//     border-top-color: var(--font-color);
+//     border-right-color: var(--font-color);
+//     border-bottom-color: var(--font-color);
+//   }
+  
+//   .number-3-bottom {
+//     border-top-color: var(--font-color);
+//     border-right-color: var(--font-color);
+//     border-bottom-color: var(--font-color);
+//   }
+  
+//   .number-4-top {
+//     border-right-color: var(--font-color);
+//     border-bottom-color: var(--font-color);
+//     border-left-color: var(--font-color);
+//   }
+  
+//   .number-4-bottom {
+//     border-top-color: var(--font-color);
+//     border-right-color: var(--font-color);
+//   }
+  
+//   .number-5-top {
+//     border-top-color: var(--font-color);
+//     border-bottom-color: var(--font-color);
+//     border-left-color: var(--font-color);
+//   }
+  
+//   .number-5-bottom {
+//     border-top-color: var(--font-color);
+//     border-right-color: var(--font-color);
+//     border-bottom-color: var(--font-color);
+//   }
+  
+//   .number-6-top {
+//     border-top-color: var(--font-color);
+//     border-bottom-color: var(--font-color);
+//     border-left-color: var(--font-color);
+//   }
+  
+//   .number-6-bottom {
+//     border-top-color: var(--font-color);
+//     border-right-color: var(--font-color);
+//     border-bottom-color: var(--font-color);
+//     border-left-color: var(--font-color);
+//   }
+  
+//   .number-7-top {
+//     border-top-color: var(--font-color);
+//     border-right-color: var(--font-color);
+//     border-left-color: var(--font-color);
+//   }
+  
+//   .number-7-bottom {
+//     border-right-color: var(--font-color);
+//   }
+  
+//   .number-8-top {
+//     border-top-color: var(--font-color);
+//     border-right-color: var(--font-color);
+//     border-bottom-color: var(--font-color);
+//     border-left-color: var(--font-color);
+//   }
+  
+//   .number-8-bottom {
+//     border-top-color: var(--font-color);
+//     border-right-color: var(--font-color);
+//     border-bottom-color: var(--font-color);
+//     border-left-color: var(--font-color);
+//   }
+  
+//   .number-9-top {
+//     border-top-color: var(--font-color);
+//     border-right-color: var(--font-color);
+//     border-bottom-color: var(--font-color);
+//     border-left-color: var(--font-color);
+//   }
+  
+//   .number-9-bottom {
+//     border-top-color: var(--font-color);
+//     border-right-color: var(--font-color);
+//     border-bottom-color: var(--font-color);
+//   }
+  
+//   .seperator-container{
+//     height: 80%;
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: space-evenly;
+//     align-items: center;
+//   }
+  
+//   .seperator{
+//     background-color: var(--font-color);
+//     border-radius: var(--border-size);
+//     height: var(--border-size);
+//     width: var(--border-size);
+//   }
